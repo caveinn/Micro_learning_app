d7e7d192cca7804dc262d50306f039139fa6cc97
caveinn <caveinncicad@gmail.com>
2019-05-15 10:24:08 +0300

    Enable  login [Finishes #165994787]

--- app.rb
+++ app.rb
@@ -30,6 +30,11 @@ def get_entry tablename, value_hash
     entry[0]
 end
 
+def update_entry tablename, update_hash, identity_hash
+    statement = "UPDATE #{tablename} SET #{update_hash.keys[0]}='#{update_hash.values[0]}' WHERE #{identity_hash.keys[0]}='#{identity_hash.values[0]}' "
+    puts statement
+    $conn.exec(statement)
+end
 
 get "/" do
    erb :home
@@ -55,16 +60,28 @@ get "/login" do
     erb :login
 end
 
+get "/profile" do 
+    user_name = session[:name]
+    @user = get_entry('users', {"user_name" => user_name })
+    erb :profile
+    #erb :profile 
+end
+
+put "/upgrade" do
+    identity_hash = {"user_name" => session[:name]}
+    update_hash ={"role" => "tutor"} 
+    update_entry("users", update_hash, identity_hash)
+end
+
 post "/login" do 
    user =  get_entry "users", {"user_name" => params["username"]}
    password = BCrypt::Password.new(user["password"])
    if password== params["password"]
     session[:name] = user["user_name"]
-    redirect "/"
+    redirect "/profile"
    else
     erb :error
    end
-   
 end
 
 
